0a0b0c316d73334c8057f1bd166c40db3d0ca50a
ikekyo <ike.kyo0802@gmail.com>
2020-04-26 17:56:36 +0900

    addTask

--- pages/index.js
+++ pages/index.js
@@ -1,51 +1,67 @@
 import * as React from 'react'
 import Board from 'react-trello'
+import { Box, Button, TextField } from '@material-ui/core'
+import data from '../data.json'
+import { makeStyles } from '@material-ui/core/styles'
 
-const data = {
-  lanes: [
-    {
-      id: 'lane1',
-      title: 'Planned Tasks',
-      label: '2/2',
-      cards: [
-        { id: 'Card1', title: 'Write Blog', description: 'Can AI make memes', label: '30 mins', metadata: { sha: 'be312a1' } },
-        { id: 'Card2', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: { sha: 'be312a1' } },
-        { id: 'Card3', title: 'Pay Rent', description: 'Transfer via NEFT', label: '5 mins', metadata: { sha: 'be312a1' } }
-      ]
-    },
-    {
-      id: 'lane2',
-      title: 'Completed',
-      label: '0/0',
-      cards: []
-    }
-  ]
-}
 let eventBus = undefined
 
 const setEventBus = (handle) => {
   eventBus = handle
 }
-//To add a card
-eventBus.publish({ type: 'ADD_CARD', laneId: 'COMPLETED', card: { id: "M1", title: "Buy Milk", label: "15 mins", description: "Also set reminder" } })
-
-//To remove a card
-eventBus.publish({ type: 'REMOVE_CARD', laneId: 'PLANNED', cardId: "M1" })
-
-//To move a card from one lane to another. index specifies the position to move the card to in the target lane
-eventBus.publish({ type: 'MOVE_CARD', fromLaneId: 'PLANNED', toLaneId: 'WIP', cardId: 'Plan3', index: 0 })
-
-//To update the lanes
-eventBus.publish({ type: 'UPDATE_LANES', lanes: newLaneData })
-
-
-export default class App extends React.Component {
-  render () {
-    return (
-      <>
-        <Board data={data} />
-        <Board data={data} eventBusHandle={setEventBus} />
-      </>
-    )
+const useStyles = makeStyles(() => ({
+  form: {
+    margin: '10px',
+  },
+  description: {
+    marginLeft: '10px'
   }
+}))
+const App = () => {
+  const [taskName, setTaskName] = React.useState('')
+  const [taskDescription, setTaskDescription] = React.useState('')
+  const classes = useStyles()
+  const addTaskHandler = (event) => {
+    eventBus.publish({
+      type: 'ADD_CARD',
+      laneId: 'PLANNED',
+      card: {
+        id: Date.now(),
+        title: taskName,
+        label: '30 mins',
+        description: taskDescription,
+      },
+    })
+  }
+
+  const setText = (event) => {
+    setTaskName(event.target.value)
+  }
+  return (
+    <>
+      <Box m={1} display="flex" className={classes.form}>
+        <form>
+          <TextField
+            label="タスク名"
+            variant="standard"
+            onChange={(event) => {
+              setTaskName(event.target.value)
+            }} />
+          <TextField
+            label="タスク詳細"
+            variant="standard"
+            onChange={(event) => {
+              setTaskDescription(event.target.value)
+            }}
+            className={classes.description}
+          />
+          <Button variant="contained" color="primary" onClick={addTaskHandler}>登録</Button>
+        </form>
+      </Box>
+      <Board data={data} eventBusHandle={setEventBus} />
+    </>
+  )
 }
+
+
+export default App
